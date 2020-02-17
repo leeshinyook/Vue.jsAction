c47df661aad60aed84074d18ec0f14a018ce52e2
leeshinyook <tlsdbr97@gmail.com>
2020-02-18 03:10:42 +0900

    Add : JSX

---
+++ chap9/jsx-example/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <HelloWorld header="1" name="에릭"></HelloWorld>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ chap9/jsx-example/src/components/HelloWorld.vue
@@ -0,0 +1,47 @@
+<template>
+<div></div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  render(h) { // jsx 렌더 함수
+    return (
+      <div><div class="text-center"
+              on-click={this.pressed}
+              domPropsInnerHTML={this.msg}></div>
+      </div>
+    )
+  },
+  data() {
+    return {
+      welcome : '안녕하세요',
+      msg: '<h${this.header}>안녕하세요 ${this.name}</h${this.header>'
+    }
+  },
+  methods: {
+    pressed() {
+      alert('클릭됨')
+    }
+  },
+  props: ['header', 'name'] // 헤더와 속성의 이름
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
